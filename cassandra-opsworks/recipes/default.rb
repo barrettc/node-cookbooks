e3be7bb5c5a5f5318c23b4545cf1c92fa820bd52
JK <jonathan.kamke@gmail.com>
2015-05-21 15:35:38 -0700

    add cassandra

---
+++ cassandra-opsworks/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: cassandra-opsworks
+# Recipe:: default
+#
+# Copyright 2015, Scispike, Inc.
+
+node['opsworks']['instance']['layers'].each do |layer|
+	Chef::Log.info("set cluster_name for #{layer}.")
+	node[:cassandra][:cluster_name] = node['opsworks']['layers'][layer]['name']
+end
+
+node[:opsworks][:instance][:layers].each do |layer,config|
+	node[:cassandra][:seeds] = node[layer]["node"]["ips"]
+end
+
+
+
+
