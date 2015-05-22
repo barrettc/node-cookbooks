fd72260f5085257f7a6663963da69652ce1583f5
JK <jonathan.kamke@gmail.com>
2015-05-21 18:19:31 -0700

    no readme

--- cassandra-opsworks/metadata.rb
+++ cassandra-opsworks/metadata.rb
@@ -2,7 +2,6 @@ name             'cassandra-opsworks'
 maintainer       'Jonathan Kamke'
 maintainer_email 'jonathan.kamke@scispike.com'
 description      'Prepares an OpsWorks stack to run the cassandra recipe'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
 depends 'cassandra'
