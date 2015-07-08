6222edd6c8c931c2dc0dc5314c7689e6097e127e
Barrett Cervenka <barrett@cervenka.com>
2015-07-08 12:54:10 -0500

    Cassandra fixes from talking to Jonathan. Fix to allow VPC to download archive from S3

--- opsworks_cassandra/recipes/default.rb
+++ opsworks_cassandra/recipes/default.rb
@@ -4,9 +4,11 @@
 #
 # Copyright 2015, Scispike, Inc.
 
-node['opsworks']['instance']['layers'].each do |layer|
-  Chef::Log.info("set cluster_name for #{layer}.")
-  node.normal[:cassandra][:cluster_name] = node['opsworks']['layers'][layer]['name']
+node[:opsworks][:instance][:layers].each do |layer|
+  if node[:cassandra][:opsworks][:seedLayer] != layer
+    Chef::Log.info("set cluster_name for #{layer}.")
+    node.normal[:cassandra][:cluster_name] = node[:opsworks][:layers][layer][:name]
+  end
 end
 
 seeds = []
